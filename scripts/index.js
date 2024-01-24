354bac2a1651e4fe1bc37a12919128c058c05559
ykkhmrdn <ykkhmrdn@gmail.com>
2024-01-25 02:20:41 +0700

    Add hero section and reasons section to index.html

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,13 @@ const navSlide = () => {
 }
 
 navSlide();
+
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
